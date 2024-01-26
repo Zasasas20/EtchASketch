522ba7785259fbbc068386818b960065c2608483
Zasasas20 <zaidassim2013@gmail.com>
2024-01-26 16:03:30 +0000

    Fixed bug now central + has title and darkening buttons

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 set(64);
 
+function toRGBObject(rgbStr) {
+    const [red, green, blue] = rgbStr.match(/\d+/g).map(Number);
+    return { red, green, blue };
+};
+
+function toRGBString(rgbObj){
+    return ("rgb(" + rgbObj.red + "," + rgbObj.green + "," + rgbObj.blue + ")");
+}
+
 function set(definition){
     document.querySelector(".containerHeaven").remove();
     document.documentElement.style.setProperty("--grid-percentage", ((100/definition) + "%"));
@@ -11,6 +20,16 @@ function set(definition){
         let box = document.createElement("div");
         box.setAttribute("class", "bockes");
 
+        box.style.backgroundColor = "rgb(245, 245, 245)";
+
+        box.addEventListener("mouseenter", (event) => {
+            let str = event.target.style.backgroundColor;
+            let color = toRGBObject(str);
+            color.red -= 25;
+            color.green -= 25;
+            color.blue -= 25;
+            event.target.style = "background-color: " + toRGBString(color);
+        });
         container.append(box);
     }
     document.body.insertBefore(container, document.querySelector(".footer"));
